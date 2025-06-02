4cdfb2de7d5c8a194f91a119ee0637dbe9c7f180
wefliwhef <6243159+kjjeon5204@users.noreply.github.com>
2025-06-02 12:39:13 -0400

    dodge mechanic

--- Assets/Script/Skill/DodgeSkillData.cs
+++ Assets/Script/Skill/DodgeSkillData.cs
@@ -7,6 +7,8 @@ namespace ProjectS.Skill
     [CreateAssetMenu(fileName = "DodgeSkillData", menuName = "Scriptable Objects/DodgeSkillData")]
     public class DodgeSkillData : AbstractSkillData
     {
+        public SkillAnimationData AnimationData;
+
         public override AbstractRuntimeSkillData GenerateRuntimeSkillData(PlayerCharacter character)
         {
             return new DodgeRuntimeSkillData(character, this);
